0507d8ddeb23019330ab1f9b65752e35e0b0c098
Gilberto Medrano <gmedrano@gmail.com>
2011-01-08 16:53:33 -0800

    this is the first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roadmap::Application.config.secret_token = '23dabfbe84138709efa533ef3dae9a434b959bef6661f4fc0e1e36941794425c9c4f01e70b2ffa02c877da05ba439eeef6b32a92a65d2417d77fca0548e7ae47'
